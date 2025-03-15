6158b48e3d0f59c5acdf9356b11d02fd6d6dd0f9
wojciech941 <skezz-perry@yandex.ru>
2025-03-15 23:44:38 +0300

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,24 +1,31 @@
 local list_t = (function()
-  local EMPTY = {}
+	local CONST_EMPTY_NODE_DATA = {}
 
-  local forward_iterator_t = (function()
+	local forward_iterator_t = (function()
     local this = {}
     local meta = {}
 
-    function meta:__index()
-      return rawget(self, "__data")
+    function meta:__call()
+    	if self and self.__data and self.__data[0] ~= CONST_EMPTY_NODE_DATA then
+				local temp = self.__data
+				self = self + 1
+				return temp
+			end
+    end
+
+    function meta:__index(k)
+    	if k == "__basetype" then
+    		return "iterator_t"
+    	elseif k == "__type" then
+    		return "forward_iterator_t"
+    	else
+     		return rawget(self, "__data")
+     	end
     end
 
     function meta:__add()
       if self.__data then
         self.__data = self.__data + 1
-      end
-      return self.__data and self
-    end
-
-    function meta:__sub()
-      if self.__data then
-        self.__data = self.__data - 1
       end
       return self.__data and self
     end
@@ -31,18 +38,7 @@ local list_t = (function()
       if data then
         local forward_iterator = {}
         forward_iterator.__data = data
-        return setmetatable(forward_iterator, {
-          __index = meta.__index,
-          __add = meta.__add,
-          __sub = meta.__sub,
-          __eq = meta.__eq,
-          __call = coroutine.wrap(function(self)
-            while self and self.__data[0] ~= EMPTY do
-              coroutine.yield(self.__data)
-              self = self + 1
-            end
-          end)
-        })
+        return setmetatable(forward_iterator, meta)
       end
     end
 
@@ -53,20 +49,27 @@ local list_t = (function()
     local this = {}
     local meta = {}
 
+    function meta:__call()
+    	if self and self.__data[0] ~= CONST_EMPTY_NODE_DATA then
+				local temp = self.__data
+				self = self + 1
+				return temp
+			end
+    end
+
     function meta:__index()
-      return rawget(self, "__data")
+      if k == "__basetype" then
+    		return "iterator_t"
+    	elseif k == "__type" then
+    		return "reverse_iterator_t"
+    	else
+     		return rawget(self, "__data")
+     	end
     end
 
     function meta:__add()
       if self.__data then
         self.__data = self.__data - 1
-      end
-      return self.__data and self
-    end
-
-    function meta:__sub()
-      if self.__data then
-        self.__data = self.__data + 1
       end
       return self.__data and self
     end
@@ -79,18 +82,7 @@ local list_t = (function()
       if data then
         local reverse_iterator = {}
         reverse_iterator.__data = data
-        return setmetatable(reverse_iterator, {
-          __index = meta.__index,
-          __add = meta.__add,
-          __sub = meta.__sub,
-          __eq = meta.__eq,
-          __call = coroutine.wrap(function(self)
-            while self and self.__data[0] ~= EMPTY do
-              coroutine.yield(self.__data)
-              self = self + 1
-            end
-          end)
-        })
+        return setmetatable(reverse_iterator, meta)
       end
     end
 
@@ -112,11 +104,23 @@ local list_t = (function()
     end
 
     function this:push_back(node)
-      self.__next, node.__next, node.__prev = node, self.__next, self
+    	if self.__next then
+    		self.__next.__prev = node
+    	end
+    	node.__next = self.__next
+    	self.__next = node
+    	node.__prev = self
+    	return node
     end
 
     function this:push_front(node)
-      self.__prev, node.__next, node.__prev = node, self, self.__prev
+    	if self.__prev then
+    		self.__prev.__next = node
+    	end
+    	node.__prev = self.__prev
+    	self.__prev = node
+    	node.__next = self
+      return node
     end
 
     function this:pop()
@@ -133,157 +137,238 @@ local list_t = (function()
     return setmetatable(this, { __call = constructor })
   end)()
 
-  local this = {}
+	local this = {}
 
-  function this:push_back(data)
-    local node = node_t(data)
-    if self.__size == 0 then
-      self.__impl.__next, self.__impl.__prev = node, node
-      self.__impl.__next.__prev, self.__impl.__prev.__next = self.__impl, self.__impl
-    else
-      self.__impl.__prev:push_back(node)
-      self.__impl.__prev = node
-    end
-    self.__size = self.__size + 1
+  function this:assign()
   end
 
-  function this:push_front(data)
-    local node = node_t(data)
-    if self.__size == 0 then
-      self.__impl.__next, self.__impl.__prev = node, node
-      self.__impl.__next.__prev, self.__impl.__prev.__next = self.__impl, self.__impl
-    else
-      self.__impl.__next:push_front(node)
-      self.__impl.__next = node
-    end
-    self.__size = self.__size + 1
+  ---@return [node_t] back element
+  function this:back()
+  	return self.__impl.__prev
   end
 
-  function this:pop_back()
-    if self.__size == 1 then
-      self:clear()
-    elseif self.__size > 1 then
-      local new_tail_node = self.__impl.__prev.__prev
-      self.__impl.__prev:pop()
-      self.__impl.__prev = new_tail_node
-      self.__size = self.__size - 1
-    end
-  end
-
-  function this:pop_front()
-    if self.__size == 1 then
-      self:clear()
-    elseif self.__size > 1 then
-      local new_head_node = self.__impl.__next.__next
-      self.__impl.__next:pop()
-      self.__impl.__next = new_head_node
-      self.__size = self.__size - 1
-    end
-  end
-
+  ---@return [forward_iterator_t]
   function this:begin()
-    return forward_iterator_t(self.__impl.__next)
+  	return forward_iterator_t(self.__impl.__next)
   end
 
-  function this:rbegin()
-    return reverse_iterator_t(self.__impl.__prev)
+  function this:cbegin()
   end
 
   function this:cend()
-    return forward_iterator_t(self.__impl)
-  end
-
-  function this:rend()
-    return reverse_iterator_t(self.__impl)
-  end
-
-  function this:front()
-    return self.__impl.__next or {}
-  end
-
-  function this:back()
-    return self.__impl.__prev or {}
   end
 
   function this:clear()
-    self.__impl.__next, self.__impl.__prev = nil, nil
-    self.__size = 0
+  	self.__impl.__next = nil
+  	self.__impl.__prev = nil
+  	self.__size = 0
   end
 
-  function this:size()
-    return self.__size
+  function this:crbegin()
   end
 
+  function this:crend()
+  end
+
+  function this:emplace()
+  end
+
+  function this:emplace_back()
+  end
+
+  function this:emplace_front()
+  end
+
+  ---@return [boolean] size ~= 0
   function this:empty()
-    return self.__size == 0
+  	return self.__size == 0
   end
 
+  ---@return [forward_iterator_t]
+  function this:end_()
+  	return forward_iterator_t(self.__impl)
+  end
+
+  function this:erase()
+  end
+
+  ---@return [node_t] front element
+  function this:front()
+  	return self.__impl.__next
+  end
+
+  -- does NOT WORK with uninitialized list
+  ---@any_iterator_t where
+  ---@table initializer_list
+  ---
+  ---@any_iterator_t where
+  ---@any value
+  ---
+  ---@any_iterator_t where
+  ---@number count 
+  ---@any value
+  ---
+  ---@return [any_iterator_t]
+  function this:insert(where, ...)
+  	local args = { ... }
+  	if #args == 1 then
+  		if type(args[1]) == "table" then
+  			for _, v in ipairs(args[1]) do
+  				where.__data:push_back( node_t(v) )
+  				where = where + 1
+  			end
+  		else
+  			where.__data:push_back( node_t(args[1]) )
+  			where = where + 1
+  		end
+  	elseif #args == 2 then
+  		for i = 1, args[1] do
+  			where.__data:push_back( node_t(args[2]) )
+  			where = where + 1
+  		end
+  	end
+  	return where
+  end
+
+  function this:merge(lambda)
+  end
+
+  function this:pop_back()
+  	if self.__impl.__prev then
+  		self.__impl.__prev:pop()
+  		self.__size = self.__size - 1
+  	end
+  end
+
+  function this:pop_front()
+  	if self.__impl.__next then
+  		self.__impl.__next:pop()
+  		self.__size = self.__size - 1
+  	end
+  end
+
+  ---@any data
+  function this:push_back(data)
+  	local node = node_t(data)
+  	if self.__impl.__prev then
+  		self.__impl:push_front(node)
+  	else
+  		self.__impl.__prev = self.__impl:push_back(node)
+  	end
+  	self.__size = self.__size + 1
+  end
+
+  ---@any data
+  function this:push_front(data)
+  	local node = node_t(data)
+  	if self.__impl.__next then
+  		self.__impl:push_back(node)
+  	else
+  		self.__impl.__next = node
+  	end
+  	self.__size = self.__size + 1
+  end
+
+  ---@return [reverse_iterator_t]
+  function this:rbegin()
+  	return reverse_iterator_t(self.__impl.__prev)
+  end
+
+  ---@any data
   function this:remove(data)
-    local node = self.__impl.__next
-    while node ~= self.__impl do
-      if node[0] == data then
-        if self.__impl.__next == node then
-          self.__impl.__next = node.__next
-        elseif self.__impl.__prev == node then
-          self.__impl.__prev = node.__prev
-        end
-        node:pop()
-        self.__size = self.__size - 1
-      end
-      node = node.__next
-    end
+  	local node = self.__impl.__next
+  	while node ~= self.__impl do
+  		if node[0] == data then
+  			node:pop()
+  			self.__size = self.__size - 1
+  		end
+  		node = node.__next
+  	end
   end
 
+  ---@function lambda
   function this:remove_if(lambda)
-    local node = self.__impl.__next
-    while node ~= self.__impl do
-      if lambda(node[0]) then
-        if self.__impl.__next == node then
-          self.__impl.__next = node.__next
-        elseif self.__impl.__prev == node then
-          self.__impl.__prev = node.__prev
-        end
-        node:pop()
-        self.__size = self.__size - 1
-      end
-      node = node.__next
-    end
+  	local node = self.__impl.__next
+  	while node ~= self.__impl do
+  		if lambda(node[0]) then
+  			node:pop()
+  			self.__size = self.__size - 1
+  		end
+  		node = node.__next
+  	end
+  end
+
+  ---@return [reverse_iterator_t]
+  function this:rend()
+  	return reverse_iterator_t(self.__impl)
   end
 
   function this:reverse()
-    if self.__size > 1 then
-      local node = self.__impl.__next
-      while node do
-        node.__prev, node.__next = node.__next, node.__prev
-        node = node.__prev
-      end
-      self.__impl.__next, self.__impl.__prev = self.__impl.__prev, self.__impl.__next
+  	local node = self.__impl.__next
+    while node ~= self.__impl do
+      node.__prev, node.__next = node.__next, node.__prev
+      node = node.__prev
     end
+    self.__impl.__next, self.__impl.__prev = self.__impl.__prev, self.__impl.__next
+  end
+
+  ---@return [number] size
+  function this:size()
+  	return self.__size
+  end
+
+  function this:sort()
+  end
+
+  function this:splice()
   end
 
   function this:swap(other)
-    self.__impl, other.__impl = other.__impl, self.__impl
+  	self.__impl, other.__impl = other.__impl, self.__impl
   end
 
-  local function constructor(_, ...)
-    local args = { ... }
-    local t = {}
-    t.__impl = node_t(EMPTY)
-    t.__size = 0
-    setmetatable(t, { __index = this })
+  function this:unique(lambda)
+  end
+
+  ---@nil empty initializer
+  ---
+  ---@table initializer_list
+  ---
+  ---@any_iterator_t first
+  ---@any_iterator_t last
+  ---
+  ---@(...) arguments initializer
+  ---
+  ---@return [list_t]
+	local function constructor(_, ...)
+		local args = { ... }
+    local list = {}
+    list.__impl = node_t(CONST_EMPTY_NODE_DATA)
+    list.__size = 0
+    setmetatable(list, { __index = this })
 
     if #args == 1 and type(args[1]) == "table" then
       for _, v in ipairs(args[1]) do
-        t:push_back(v)
+        list:push_back(v)
       end
+    elseif
+    	#args == 2 and
+    	(type(args[1]) == "table" and args[1].__basetype == "iterator_t") and
+    	(type(args[2]) == "table" and args[2].__basetype == "iterator_t")
+    then
+    	local iterator = args[1]
+    	while iterator ~= args[2] do
+    		list:push_back(iterator[0][0])
+    		iterator = iterator + 1
+    	end
     elseif #args > 0 then
       for _, v in ipairs(args) do
-        t:push_back(v)
+        list:push_back(v)
       end
     end
 
-    return t
-  end
+    return list
+	end
 
-  return setmetatable(this, { __call = constructor })
+	return setmetatable(this, { __call = constructor })
 end)()
